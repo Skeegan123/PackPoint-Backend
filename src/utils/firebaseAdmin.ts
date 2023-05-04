7d29bac84bf22d688f3723abd7ab29cf029e97f5
Skeegan123 <mcfeegan123@gmail.com>
2023-05-04 16:25:21 -0600

    idek

--- src/utils/firebaseAdmin.ts
+++ src/utils/firebaseAdmin.ts
@@ -5,14 +5,6 @@ const https = require("https");
 const fs = require("fs");
 const path = require("path");
 
-const blobUrl = "https://packpoint.blob.core.windows.net/app-info/packpoint-5fba2-firebase-adminsdk-svi4j-d958796224.json";
-
-https.get(blobUrl, (res: { pipe: (arg0: any) => void }) => {
-  const filePath = path.resolve(__dirname, "../../packpoint-5fba2-firebase-adminsdk-svi4j-d958796224.json");
-  const fileStream = fs.createWriteStream(filePath);
-  res.pipe(fileStream);
-});
-
 import * as serviceAccount from "../../packpoint-5fba2-firebase-adminsdk-svi4j-d958796224.json";
 
 try {
