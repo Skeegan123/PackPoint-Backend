ba79784becd72addbc79bdd8358cdda867a222a9
Skeegan123 <mcfeegan123@gmail.com>
2023-05-04 15:38:40 -0600

    Azure Test

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,37 +1,3 @@
-// import pgPromise from "pg-promise";
-// import logger from "../utils/logger";
-// import { exit } from "process";
-
-// const pgp = pgPromise({});
-
-// const connection = {
-//   host: process.env.DB_HOST,
-//   port: parseInt(process.env.DB_PORT || "5432"),
-//   database: process.env.DB_NAME,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// };
-
-// logger.info("Connecting to database...");
-
-// const db = pgp(connection);
-// db.one("SELECT $1 AS value", [123])
-//   .then((data) => {
-//     // success;
-//     logger.info("Connected to database");
-//   })
-//   .catch((error) => {
-//     // error;
-//     logger.error("Unable to connect to database:", error);
-//     // logger.info("Exiting application");
-//     // exit(1);
-//   });
-
-// export default db;
-
 import pgPromise from "pg-promise";
 import logger from "../utils/logger";
 import { exit } from "process";
--- src/utils/firebaseAdmin.ts
+++ src/utils/firebaseAdmin.ts
@@ -1,8 +1,14 @@
 import * as admin from "firebase-admin";
-import * as serviceAccount from "../../packpoint-5fba2-firebase-adminsdk-svi4j-d958796224.json";
+import * as serviceAccount from "/packpoint-5fba2-firebase-adminsdk-svi4j-d958796224.json";
+import logger from "../utils/logger";
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-});
+try {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  });
+  logger.info("Firebase admin initialized");
+} catch (error) {
+  logger.error("Firebase admin initialization failed", error);
+}
 
 export default admin;
